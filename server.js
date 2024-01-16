0f97850e1bc2ad5df804e47c954d0028d8c41c36
TheSaviourEking <saviour.eking@outlook.com>
2024-01-16 04:43:40 +0100

    fix code to simulate expected result

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
 // Your code here
+const http = require('http');
+
+const PORT = 5000;
+const body = "<h1>Hello there!</h1>";
+const code = `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World!</title>
+  </head>
+  <body>
+    ${body}
+  </body>
+  </html>
+`;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.write(code);
+    res.end();
+});
+server.listen(PORT, () => console.log('Server is listening on port', PORT));
